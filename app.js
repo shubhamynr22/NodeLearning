9b277ae90e52c123b7459e97bb4e3326d46840f7
shubhamynr22 <shubhamynr22@gmail.com>
2023-07-23 02:54:01 +0530

    routing requests

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method, req.headers);
+    //console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><h1>Hello from the other side !!</h1><form action = "/message" method = "POST" ><input type = "text" name ="message"><button type = "submit">Click Here!</button></body>');
+        res.write('</html>');
+        return res.end();  
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Shubhams Response</title></head>');
-    res.write('<body><h1>Hello from the other side !!</h1></body>');
+    res.write('<body><h1>You are on the new page</h1></body>');
     res.write('</html>');
     res.end();
 })

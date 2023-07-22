031c646fc76dd73a57905f58b9034a1138a1c809
shubhamynr22 <shubhamynr22@gmail.com>
2023-07-23 03:08:37 +0530

    redirecting requests

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
     //console.log(req.url, req.method, req.headers);
     const url = req.url;
+    const method = req.method;
     if(url === '/'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
@@ -10,6 +12,12 @@ const server = http.createServer((req, res)=>{
         res.write('<body><h1>Hello from the other side !!</h1><form action = "/message" method = "POST" ><input type = "text" name ="message"><button type = "submit">Click Here!</button></body>');
         res.write('</html>');
         return res.end();  
+    }
+    if (url === '/message' && method == 'POST') {
+        fs.writeFileSync('message.txt', 'Ye Boi!');
+        res.statusCode =302;
+        res.setHeader('Location', '/');
+        return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');

8560775b08a3f1a993821aaab164f4a07066f09f
shubhamynr22 <shubhamynr22@gmail.com>
2023-07-23 02:31:24 +0530

    sending response

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log(req);
-    process.exit();
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Shubhams Response</title></head>');
+    res.write('<body><h1>Hello from the other side !!</h1></body>');
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(3000);

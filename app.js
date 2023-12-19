225bfe554d44cace97ef17418126405b55d1c355
shubhamynr22 <shubhamynr22@gmail.com>
2023-12-20 03:30:55 +0530

    feat : added a lot

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const http = require("http");
-const requestHandler = require("./routes");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 
-const server = http.createServer(requestHandler);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+});
+
+app.listen(3000);

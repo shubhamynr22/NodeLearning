2616227b60524ab2c3f3764913961fab4ce8227b
shubhamynr22 <shubhamynr22@gmail.com>
2023-12-19 16:20:03 +0530

    feat : adding modules

--- app.js
+++ app.js
@@ -1,42 +1,6 @@
-const http = require('http');
-const fs = require('fs');
-const { parse } = require('path');
+const http = require("http");
+const requestHandler = require("./routes");
 
-const server = http.createServer((req, res)=>{
-    //console.log(req.url, req.method, req.headers);
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><h1>Hello from the other side !!</h1><form action = "/message" method = "POST" ><input type = "text" name ="message"><button type = "submit">Click Here!</button></body>');
-        res.write('</html>');
-        return res.end();  
-    }
-    if (url === '/message' && method == 'POST') {
-        const body = [];
-        req.on('data', (chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        })
-        req.on('end', ()=>{
-            const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
-            const message = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        })
-        fs.writeFileSync('message.txt', 'Ye Boi!');
-        res.statusCode =302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Shubhams Response</title></head>');
-    res.write('<body><h1>You are on the new page</h1></body>');
-    res.write('</html>');
-    res.end();
-})
+const server = http.createServer(requestHandler);
 
 server.listen(3000);

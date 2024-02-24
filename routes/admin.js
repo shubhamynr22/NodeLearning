30615e7466ada11132c34359d8c6f62e685d2289
shubhamynr22 <shubhamynr22@gmail.com>
2024-02-25 02:44:28 +0530

    feat : added pug templating engine to shop

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,17 @@ const path = require('path');
 const rootDir = require('../util/path');
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
   console.log(req.body);
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
